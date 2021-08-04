525334dc0eb45605c11c736379a67f1e21563ce4
unknown <vhuang5564@gmail.com>
2021-08-04 14:06:53 -0700

    add interviewerClass, select, clickable to storybook

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "components/InterviewerListItem.scss";
+import classnames from 'classnames';
 
 
 export default function InterviewerListItem(props){
+
+  const interviewerClass = classnames("interviewers", {
+    'interviewers__item':true,
+    'interviewers__item--selected': props.selected,
+  })
+
   return (
-    <li className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.name)}className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
