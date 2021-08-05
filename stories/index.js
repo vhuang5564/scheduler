48213afc81e06f07f48e37df82fc891f2d3779d3
unknown <vhuang5564@gmail.com>
2021-08-04 19:37:45 -0700

    add Confirm, Error, Show, Status components and stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,11 +142,33 @@ storiesOf("InterviewerList", module)
 
   //////////////////////////////////////////////////////////////////////////
 
-  storiesOf("Appointment", module)
-    .addParameters({
-      backgrounds: [{ name: "white", value: "#fff", default: true }]
-    })
-    .add("Appointment", () => <Appointment />)
-    .add("Appointment with Time", () => <Appointment time="12pm" />)
-    .add("Header", () => <Header time="12pm"/>)
-    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show 
+      student="Lydia Miller-Jones"
+      interviewer={interviewers[0].name} // retrieves interviewer 0 from line 115
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}  
+    />
+  ))
+  .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => (
+    <Error
+      message="Could not Delete Appointment"
+      onClose={action("onClose")}
+    />
+  ))
