9e681abd0d60be67482383ae8adea8cd5abc82dc
unknown <vhuang5564@gmail.com>
2021-08-08 10:21:44 -0700

    Incorporate useVisualMode functions to display EMPTY,SHOW and FORM

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,64 +6,7 @@ import Appointment from "./Appointment";
 import axios from "axios";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Bob Ross",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Simba",
-//       interviewer: {
-//         id: 2,
-//         name: "Jafar",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Aladdin",
-//       interviewer: {
-//         id: 3,
-//         name: "Genie",
-//         avatar: "https://i.imgur.com/T2WwVfS.png",
-//       }
-//     }
-//   }
-// ];
-
 export default function Application(props) {
-  const appointments = {};
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -73,15 +16,6 @@ export default function Application(props) {
 
   const setDay = day => setState({...state, day });
   let dailyAppointments = [];
-
-  // const setDays = days => setState(prev => ({ ...prev, days }));
-
-  // useEffect(() => {
-  //   axios.get("http://localhost:8001/api/days").then((response) => {
-  //     console.log(response.data);
-  //     setDays(response.data);
-  //   })
-  // }, [])
 
   useEffect(() => {
     Promise.all([
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,16 +3,35 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-        {props.interview 
-          ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> // if props.interview == true ternary operator ? = true
-          : <Empty/> // if props.interview == false ternary operator : = false
-        }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]} // change when getInterviewersForDay is implemented
+          onCancel={() => transition(EMPTY)}
+        />
+      )}
     </article>
   )
 }
