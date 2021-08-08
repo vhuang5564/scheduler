05d9b1af1cf66983c32536146aae74cc0cb90ed0
unknown <vhuang5564@gmail.com>
2021-08-08 10:26:21 -0700

    change transition function to back in CREATE form

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -29,7 +29,7 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form 
           interviewers={[]} // change when getInterviewersForDay is implemented
-          onCancel={() => transition(EMPTY)}
+          onCancel={() => back()}
         />
       )}
     </article>
